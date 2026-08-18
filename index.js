702f1aca002eb3d61b6d1d97acf6560b2a65aadf
Ruqayya-Hasnee <ruqayya.hasnee@gmail.com>
2026-08-18 16:15:35 +0500

    connected mongodb with mongoose

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.get("/", (req, res) => {
-  res.send("helloo")
+  res.json({
+    message: "Server is running .....",
+  });
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("server is running on port 3000");
 });
