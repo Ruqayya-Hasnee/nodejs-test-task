7802abc2258b1350f3b78bb4437e958b1b817c5e
Ruqayya-Hasnee <ruqayya.hasnee@gmail.com>
2026-08-18 17:06:44 +0500

    resolve environment variables loading issue

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoutes = require("./routes/auth");
 
 dotenv.config();
+
+//route
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
