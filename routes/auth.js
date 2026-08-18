f29511e47d15f1329ec2bd548b82e11a60674b69
Ruqayya-Hasnee <ruqayya.hasnee@gmail.com>
2026-08-18 16:49:19 +0500

    added user login with jwt

--- routes/auth.js
+++ routes/auth.js
@@ -4,36 +4,61 @@ const User = require("../models/User");
 
 const router = express.Router();
 
+// register user
 router.post("/signup", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+    try {
+        const { name, email, password } = req.body;
 
-    // checking if user with this email already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({
-        message: "Email already exists",
-      });
+        // checking if user with this email already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({
+                message: "Email already exists",
+            });
+        }
+        // password hashing
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        await user.save();
+
+        res.status(201).json({
+            message: "Successfully registered",
+            user,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong",
+        });
     }
-    // password hashing
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({
-      name,
-      email,
-      password: hashedPassword,
-    });
+});
 
-    await user.save();
+//login user
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) return res.status(400).send("Email not found");
 
-    res.status(201).json({
-      message: "Successfully registered",
-      user,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: "Something went wrong",
-    });
-  }
+        const validPass = await bcrypt.compare(req.body.password, user.password);
+        if (!validPass) return res.status(400).send("Invalid password");
+
+        if (user.status !== "approved") {
+            return res.status(403).send("Verifyy your account frm admin");
+        }
+
+        const token = jwt.sign(
+            { _id: user._id, role: user.role },
+            process.env.TOKEN_SECRET
+        );
+
+        res.header("auth-token", token).send(token);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 module.exports = router;
